90cb73f8fb3696874fe6fb6a4958e17bbbc629f1
Tausif Ahmed <atausif00@gmail.com>
2014-09-16 16:14:16 -0400

    Added Xcode files for IOS version of Spoiler

---
+++ Spoiler_IOS/Spoiler_IOS/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Spoiler_IOS
+//
+//  Created by Tausif Ahmed on 9/16/14.
+//  Copyright (c) 2014 Spoiler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ Spoiler_IOS/Spoiler_IOSTests/Spoiler_IOSTests.m
@@ -0,0 +1,34 @@
+//
+//  Spoiler_IOSTests.m
+//  Spoiler_IOSTests
+//
+//  Created by Tausif Ahmed on 9/16/14.
+//  Copyright (c) 2014 Spoiler. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Spoiler_IOSTests : XCTestCase
+
+@end
+
+@implementation Spoiler_IOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
