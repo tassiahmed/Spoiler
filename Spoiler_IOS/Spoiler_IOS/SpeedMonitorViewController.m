508da3581ebd951af82b68d8dd540e526b5fe578
Evan Thompson <evanmt13@gmail.com>
2014-11-15 16:40:14 -0500

    Refactored for readability

--- Spoiler_IOS/Spoiler_IOS/SpeedMonitorViewController.m
+++ Spoiler_IOS/Spoiler_IOS/SpeedMonitorViewController.m
@@ -20,60 +20,77 @@
     
     //using loc in the header
     self.loc = [self.cllManager location];
+
     NSString * numStr = [NSString stringWithFormat:@"%.0f", [self.loc speed]];
 
     [self.lbl setText:numStr];
 }
 
-//Handles when the stop button is pressed
-- (IBAction)onStop:(id)sender {
+- (void) stopUISetup{
     //enable and disable the correct buttons
     [self.RunBtn setEnabled:YES];
     [self.StopBtn setEnabled:NO];
     //set the descriptive label to inactive
     [self.activeLabel setText:@"Inactive..."];
-    
-    //Stop updating the location.  Will save battery.
-    [self.cllManager stopUpdatingLocation];
-    self.cllManager = nil;
-    
     //remove the animation
     [[[self activeLabel] layer] removeAnimationForKey:@"pulse"];
-    //end the label timer
-    [self.lblTimer invalidate];
     [self.lbl setText:@"0"];
 }
 
-//Handles when the run button is pressed
-- (IBAction)onRun:(id)sender{
-    self.counter = 0;
+- (void) stopLocation{
+    //Stop updating the location.  Will save battery.
+    [self.cllManager stopUpdatingLocation];
+    self.cllManager = nil;
+}
+
+//Handles when the stop button is pressed
+- (IBAction)onStop:(id)sender {
+    //set the UI to the stop state
+    [self stopUISetup];
+    //set the location to a stopped state
+    [self stopLocation];
+    
+    //end the label timer
+    [self.lblTimer invalidate];
+}
+
+//sets up the initialization of all of the parts when run is called
+- (void) runInitializations{
+    
     self.lblLog = [[Log alloc] init];
-    
-    //enable and disable the correct buttons
-    [self.StopBtn setEnabled:YES];
-    [self.RunBtn setEnabled:NO];
-    
-    
-    //start updating the location
     self.cllManager = [[CLLocationManager alloc] init];
     
-    
-    //need to check for gps here later
-    //No good way of doing it at the moment
-    self.cllManager.delegate = self;
     // Check for iOS 8. Without this guard the code will crash with "unknown selector" on iOS 7.
     if ([self.cllManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
         [self.cllManager requestWhenInUseAuthorization];
     }
     
+    //set the accuracy
+    self.cllManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation;
+    
+    //begin updating
     [self.cllManager startUpdatingLocation];
     
-    //self.loc = [[CLLocation alloc] init];
-    
+}
+
+- (void) runUISetup{
+    //enable and disable the correct buttons
+    [self.StopBtn setEnabled:YES];
+    [self.RunBtn setEnabled:NO];
     //set the descriptive label to running
     [self.activeLabel setText:@"Running..."];
     //set the animation
     [[[self activeLabel] layer] addAnimation:self.anim forKey:@"pulse"];
+}
+
+//Handles when the run button is pressed
+- (IBAction)onRun:(id)sender{
+    
+    //intitalize the parts needed for log
+    [self runInitializations];
+    
+    //change the UI for the run state
+    [self runUISetup];
     
     //start the timer for updating the label
     self.lblTimer = [NSTimer scheduledTimerWithTimeInterval: .5 target:self selector: @selector(lblUpdate) userInfo:nil repeats:YES];
