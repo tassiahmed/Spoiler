360851311f8232e2e72a7ea48584fb2ad0f1ef04
Tausif Ahmed <atausif00@gmail.com>
2015-02-12 19:04:29 -0500

    Added Files for porting the app to Swift.  This is for experimental purposes to get better understanding of developing an IOS app and more

---
+++ Spoiler_Swift_Beta/Spoiler_Swift_Beta/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Spoiler_Swift_Beta
+//
+//  Created by Tausif Ahmed on 2/12/15.
+//  Copyright (c) 2015 Spoiler. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Spoiler_Swift_Beta/Spoiler_Swift_Beta/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Spoiler_Swift_Beta
+//
+//  Created by Tausif Ahmed on 2/12/15.
+//  Copyright (c) 2015 Spoiler. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
