16786fa949c0cd86de7e9336797370898093c1dd
Tausif Ahmed <atausif00@gmail.com>
2015-05-05 13:49:42 -0400

    Staging current version of app to move over to less intensive use of storyboard

--- Spoiler_IOS/Spoiler_IOS/View Controllers/Log View/LogViewController.h
+++ Spoiler_IOS/Spoiler_IOS/View Controllers/Log View/LogViewController.h
@@ -1,30 +1,5 @@
 //
 //  LogViewController.h
-//  Spoiler_IOS
-//
-//  Created by Tausif Ahmed on 10/28/14.
-//  Copyright (c) 2014 Spoiler. All rights reserved.
-//
-
-/*
-#import <UIKit/UIKit.h>
-#import <Foundation/Foundation.h>
-#import "Log.h"
-#import "SharedData.h"
-
-@interface LogViewController : UITableViewController
-
-@property NSArray *logData;
-
-@property UIRefreshControl *refresh;
-
-@property (strong, nonatomic) SharedData* sharedData;
-
-@end
-*/
-
-//
-//  LogTableViewController.h
 //  Spoiler_Test
 //
 //  Created by Tausif Ahmed on 4/20/15.
@@ -32,17 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "SharedData.h"
 
-@interface LogViewController : UINavigationController <UITableViewDataSource, UITableViewDelegate>
+@interface LogViewController : UIViewController /*<UITableViewDataSource, UITableViewDelegate>*/
 
-@property (strong, nonatomic) UITableView *log_table;
+@property NSString *file_name;
 
-@property NSArray *logData;
-
-@property NSString *selected_file;
-
-@property (strong, nonatomic) SharedData *sharedData;
-
+- (void) setFileName: (NSString* ) name;
 
 @end
