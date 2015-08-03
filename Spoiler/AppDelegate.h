ebfc2fffa9d21f9fbc5a4f1b1b7ed129abe09131
Tausif Ahmed <atausif00@gmail.com>
2015-08-02 21:41:55 -0700

    Using cocoa pods

---
+++ Spoiler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Spoiler
+//
+//  Created by Tausif Ahmed on 8/2/15.
+//  Copyright (c) 2015 Spoiler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
